7214c1a6b88cb7a10bee3f7ab3a9187133afbe9e
Gizem <mgizemykbg@gmail.com>
2021-12-24 21:56:39 +0300

    Refactor comicList

--- src/components/ComicsList/ComicsList.js
+++ src/components/ComicsList/ComicsList.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import {Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import React, {useState} from 'react';
+import {FlatList, Text} from 'react-native';
 import {ActivityIndicator, Colors} from 'react-native-paper';
 import {useGetAllComicsQuery} from '../../api/comics/';
 import MovieCard from '../MovieCard';
 
 const ComicsList = () => {
+  const navigation = useNavigation();
   const {data, error, isFetching, isLoading} = useGetAllComicsQuery();
   if (error) return <Text>No content</Text>;
   if (isFetching)
@@ -12,24 +14,33 @@ const ComicsList = () => {
   if (isLoading)
     return <ActivityIndicator animating={true} color={Colors.red800} />;
   if (!data) return null;
+
   if (data) {
-    console.log(data);
-    return <Text>{data.data.count}</Text>;
+    console.log(data.data.results);
+    return (
+      <FlatList
+        data={data.data.results}
+        keyExtractor={item => item.id.toString()}
+        renderItem={() =>
+          data.data.results.map(item => (
+            <MovieCard
+              item={item}
+              onFavPress={handleAddFavorite}
+              //   onComicPressed={() => goDetailPage(item)}
+            />
+          ))
+        }
+      />
+    );
   }
-  console.log(data);
-  const renderMovieCard = ({item}) => {
-    <MovieCard item={item} onClick={handleAddFavorite} />;
-  };
+
   function handleAddFavorite() {
     console.log('giz');
   }
-  return (
-    <FlatList
-      data={data}
-      keyExtractor={item => item.id.toString()}
-      renderItem={renderMovieCard}
-    />
-  );
+
+  // function goDetailPage(selectedComic) {
+  //   navigation.navigate(routes.COMIC_DETAIL_PAGE, {comic: selectedComic});
+  // }
 };
 
 export default ComicsList;
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,23 +1,24 @@
 import * as React from 'react';
+import {TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
 import {Button, Card, Title, Paragraph} from 'react-native-paper';
 import {Icon} from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const MovieCard = ({item, onClick}) => (
-  <Card>
-    <Card.Title title={item.title} left={LeftContent} />
-    <Card.Content>
-      <Title>{item.title}</Title>
-      <Paragraph>{item.textObjects.text}</Paragraph>
-    </Card.Content>
-    <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
-    <Card.Actions>
-      <Button
-        onClick={onClick}
-        left={props => <Button.Icon {...props} icon="favorite-border" />}>
-        Add Favorite
-      </Button>
-    </Card.Actions>
-  </Card>
+const MovieCard = ({item, onComicPressed, onFavPress}) => (
+  <TouchableOpacity onPress={onComicPressed}>
+    <Card>
+      <Card.Title title={item.title} />
+      <Card.Content>
+        <Title>{item.title}</Title>
+        <Paragraph>{item.textObjects.text}</Paragraph>
+      </Card.Content>
+      <Card.Cover source={{uri: `${item.thumbnail.path}.jpg`}} />
+      <Card.Actions>
+        {/* <TouchableOpacity onPress={onFavPress}>
+          <Icon name="favorite-border" size={30} />
+        </TouchableOpacity> */}
+      </Card.Actions>
+    </Card>
+  </TouchableOpacity>
 );
 
 export default MovieCard;
